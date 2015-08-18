bcb2c850c052b6f12a3f09319990f1a2926003fb
CupcakeMonster <flukersy@gmail.com>
2015-08-19 04:22:11 +0700

    Completed program.cpp

--- list/program.cpp
+++ list/program.cpp
@@ -9,7 +9,31 @@ void main()
 	mylist.pushToHead('k');
 	mylist.pushToHead('e');
 	mylist.pushToHead('n');
+
+	cout << "Original: ";
 	mylist.print();
+	cout << endl;
+
+	cout << "Push tail: ";
+	mylist.pushToTail('y');
+
+	mylist.print();
+	cout << endl;
+
+	mylist.popTail();
+	cout << "Pop tail: ";
+	mylist.print();
+	cout << endl;
+
+	mylist.reverse();
+	cout << "After reverse: ";
+	mylist.print();
+	cout << endl;
+
+	mylist.search('e');
+	cout << endl;
+
+	system("pause");
 
 	//TO DO! Write a program that tests your list library - the code should take characters, push them onto a list, 
 	//- then reverse the list to see if it is a palindrome!
